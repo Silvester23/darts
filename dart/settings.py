99860088f608dd4175e74835e43d212222a937c0
Jan Murmann <jan@gt-3.fritz.box>
2018-03-18 01:21:42 +0100

    ...

--- dart/settings.py
+++ dart/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'dart.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': os.environ['DATABASE_NAME'],
-        #'USER': os.environ['DATABASE_USER'],
-        #'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        #'HOST': os.environ['DATABASE_HOST'],
-        #'PORT': '5432',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': '5432',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': 'db.sqlite3',
     }
 }
 
